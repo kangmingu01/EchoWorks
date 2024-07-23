8d014ec3a151be09eff863a32ed59a6df44805c8
itwill <itwill@DESKTOP-P5VOEQ4>
2024-07-23 20:54:25 +0900

    240723_상세페이지_상품테이블 가져오기

---
+++ Echo_Works/src/main/java/echoworks/dao/ProductStockDAO.java
@@ -0,0 +1,55 @@
+package echoworks.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import echoworks.dto.ProductStockDTO;
+
+
+public class ProductStockDAO extends JdbcDAO{
+	private static ProductStockDAO _dao;
+	public ProductStockDAO() {
+		// TODO Auto-generated constructor stub
+	}
+	static {
+		_dao=new ProductStockDAO();
+	}	
+	public static ProductStockDAO getDAO() {
+		return _dao;
+	}
+	
+	//재고테이블 불러오기
+	public List<ProductStockDTO> selectProductStockAll(){
+		Connection con=null;
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		List<ProductStockDTO> productStockList=new ArrayList<ProductStockDTO>();
+		try {
+			con=getConnection();
+			String sql="select product_stock_no, product_stock_pno, product_stock_option ,product_stock_stock ,product_stock_price from product_stock order by product_stock_no";
+			pstmt=con.prepareStatement(sql);
+			rs=pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ProductStockDTO productStock=new ProductStockDTO();
+				productStock.setpS_No(rs.getInt("product_stock_no"));
+				productStock.setpS_pNo(rs.getInt("product_stock_pno"));
+				productStock.setpS_Option(rs.getNString("product_stock_option"));
+				productStock.setpS_Stock(rs.getInt("product_stock_stock"));
+				productStock.setpS_price(rs.getInt("product_stock_price"));
+				
+				productStockList.add(productStock);				
+			}						
+		}catch (SQLException e) {
+			System.out.println("[에러]selectProductStockAll() 메소드의 SQL 오류 = "+e.getMessage());
+		} finally {
+			close(con, pstmt, rs);
+		}
+		return productStockList;				
+	}
+	
+}
